9611f6e948cf724b0a4a113f6baf3bc245848f55
DavidRood <shakiel@roadrunner.com>
2017-07-27 22:09:01 -0400

    enemy formation moving, laser firing, collider to destroy objects off screen

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public float speed = 5f;
+    public float padding = 1f;
+
+    private int direction = 1;
+    private float boundaryRightEdge, boundaryLeftEdge;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Camera camera = Camera.main;
+        float distance = transform.position.z - camera.transform.position.z;
+        boundaryLeftEdge = camera.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + padding;
+        boundaryRightEdge = camera.ViewportToWorldPoint(new Vector3(1, 1, distance)).x - padding;
+
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+            //float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+            //Vector3 leftBoundry = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+            //Vector3 rightBoundry = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+            //xmax = rightBoundry.x;
+            //xmin = leftBoundry.x;
+
+            //foreach (Transform child in transform)
+            //{
+            //    GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            //    enemy.transform.parent = child;
+            //}
+        }
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0f));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float formationRightEdge = transform.position.x + 0.5f * width;
+
+        float formationLeftEdge = transform.position.x - 0.5f * width;
+
+        if (formationRightEdge > boundaryRightEdge)
+        {
+
+            direction = -1;
+
+        }
+
+        if (formationLeftEdge < boundaryLeftEdge)
+        {
+
+            direction = 1;
+
+        }
+
+        transform.position += new Vector3(direction * speed * Time.deltaTime, 0, 0);
+        //if (movingRight)
+        //   {
+        //       transform.position += Vector3.right * speed * Time.deltaTime;
+        //   }
+        //   else
+        //   {
+        //       transform.position += Vector3.left * speed * Time.deltaTime;
+        //   }
+
+        //   float rightEdgeofFormation = transform.position.x + (0.5f * width);
+        //   float leftEdgeofFormation = transform.position.x - (0.5f * width);
+        //   if(leftEdgeofFormation < xmin || rightEdgeofFormation > xmax)
+        //   {
+        //       movingRight = !movingRight;
+        //   }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,28 +3,55 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float minX, maxX;
     public float speed = 15.0f;
+    public float padding = 1f;
+    public GameObject projectile;
+    public float projectileSpeed = 5f;
+    public float firingRate = 0.2f;
+    float minX;
+    float maxX;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
+    void Start ()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(0.95f, 0f, distance));
+        minX = leftmost.x + padding;
+        maxX = rightmost.x - padding;
+    }
+    void Fire()
+    {
+            GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+
+    }
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.000001f, firingRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Left Arrow Pressed");
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            // transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Right Arrow Pressed");
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-            // transform.position.x = Mathf.Clamp(this, minX, maxX);
+            // transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
-	}
+        // restrict the player to the game space.
+        float newX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
 
 
     //Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
